6190d28d6b066bc37a4a0dab775954553f0101ae
Gabriel Cancro <gabrielcancro@3ogs.com.ar>
2022-01-27 10:10:25 -0300

    update

---
+++ Camera2D.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+var mouse_start_pos
+var screen_start_position
+var dragging = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+#	position.x += 5
+
+func _input(event):	
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			print("adasdasdasd")
+			mouse_start_pos = event.position
+			screen_start_position = position
+			dragging = true			
+		else:
+			dragging = false
+	if event is InputEventMouseMotion and dragging:
+		position = zoom * (mouse_start_pos - event.position) + screen_start_position
--- prefab/Unit.gd
+++ prefab/Unit.gd
@@ -7,10 +7,10 @@ func _ready():
 	randomize()
 	goto = Vector2(rand_range(50,1024), rand_range(50,600))
 	print(goto)
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var vecGoto = (goto - position)
 	if vecGoto.length() > 50: position += vecGoto.normalized() * delta * 200
-#	pass
+	$Light2D.texture_scale = .5+randf()*.1
+#	$Light2D.energy = .9+randf()*.2
